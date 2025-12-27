3d21733939aa1ad0ea47880d559bcb234bc5c72f
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-28 01:15:51 +0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,30 @@
+const DB_NAME = "sliderDB";
+const STORE = "images";
+let db;
 
+export function initDB() {
+  return new Promise(resolve => {
+    const req = indexedDB.open(DB_NAME, 1);
+    req.onupgradeneeded = e => {
+      db = e.target.result;
+      db.createObjectStore(STORE, { autoIncrement: true });
+    };
+    req.onsuccess = e => {
+      db = e.target.result;
+      resolve();
+    };
+  });
+}
+
+export function saveImage(blob) {
+  const tx = db.transaction(STORE, "readwrite");
+  tx.objectStore(STORE).add(blob);
+}
+
+export function getImages() {
+  return new Promise(resolve => {
+    const tx = db.transaction(STORE, "readonly");
+    const req = tx.objectStore(STORE).getAll();
+    req.onsuccess = () => resolve(req.result);
+  });
+}
