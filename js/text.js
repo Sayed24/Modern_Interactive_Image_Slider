0939d74f504ed0f6959efc1334d1c6b2101b4ef7
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-29 01:00:17 +0300

    Update text.js

--- js/text.js
+++ js/text.js
@@ -1,1 +1,14 @@
+import { state } from "./state.js";
+import { renderSlider } from "./slider.js";
 
+export function setupTextOverlay() {
+  document.getElementById("slide-text").oninput = e => {
+    state.texts[state.currentIndex] = e.target.value;
+    renderSlider();
+  };
+
+  document.getElementById("text-position").onchange = e => {
+    state.textPositions[state.currentIndex] = e.target.value;
+    renderSlider();
+  };
+}
