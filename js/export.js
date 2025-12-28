2f320985700cd986614b75e99a649e89328b6d1b
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-29 01:01:15 +0300

    Update export.js

--- js/export.js
+++ js/export.js
@@ -1,1 +1,18 @@
+import { state } from "./state.js";
 
+export function exportAsGIF() {
+  gifshot.createGIF(
+    {
+      images: state.images,
+      interval: 0.6
+    },
+    res => {
+      if (!res.error) {
+        const a = document.createElement("a");
+        a.href = res.image;
+        a.download = "slider.gif";
+        a.click();
+      }
+    }
+  );
+}
