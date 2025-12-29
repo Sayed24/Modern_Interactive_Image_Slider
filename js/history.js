44346929aed69ee49d42bc8577134a1d5b50e83e
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-30 00:15:38 +0300

    Update history.js

--- js/history.js
+++ js/history.js
@@ -1,1 +1,22 @@
+import { state } from "./state.js";
 
+const history = [];
+let index = -1;
+
+export function pushHistory() {
+  history.splice(index + 1);
+  history.push(JSON.stringify(state));
+  index++;
+}
+
+export function undo() {
+  if (index <= 0) return;
+  index--;
+  Object.assign(state, JSON.parse(history[index]));
+}
+
+export function redo() {
+  if (index >= history.length - 1) return;
+  index++;
+  Object.assign(state, JSON.parse(history[index]));
+}
