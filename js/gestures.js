1ce1263758b861a5ba7f7434e6f6f223471f99d8
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-30 00:18:56 +0300

    Update gestures.js

--- js/gestures.js
+++ js/gestures.js
@@ -1,13 +1,15 @@
-import { nextSlide, prevSlide } from "./slider.js";
+import { next, prev } from "./slider.js";
 
 export function enableSwipe(el) {
   let startX = 0;
+
   el.addEventListener("touchstart", e => {
     startX = e.touches[0].clientX;
   });
+
   el.addEventListener("touchend", e => {
     const diff = e.changedTouches[0].clientX - startX;
-    if (diff > 50) prevSlide();
-    if (diff < -50) nextSlide();
+    if (diff > 50) prev();
+    if (diff < -50) next();
   });
 }
