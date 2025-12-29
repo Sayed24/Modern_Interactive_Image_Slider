32b910cdd8adb0e3b196c291a65ef6feee567090
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-30 00:22:07 +0300

    Update exportMp4.js

--- js/exportMp4.js
+++ js/exportMp4.js
@@ -1,1 +1,22 @@
+import { createFFmpeg, fetchFile } from
+  "https://unpkg.com/@ffmpeg/ffmpeg@0.12.6/dist/ffmpeg.min.js";
+import { exportVideo } from "./exportVideo.js";
 
+const ffmpeg = createFFmpeg({ log: true });
+
+export async function exportMP4() {
+  if (!ffmpeg.isLoaded()) await ffmpeg.load();
+
+  const webmBlob = await exportVideo(true);
+
+  ffmpeg.FS("writeFile", "input.webm", await fetchFile(webmBlob));
+  await ffmpeg.run("-i", "input.webm", "output.mp4");
+
+  const data = ffmpeg.FS("readFile", "output.mp4");
+  const blob = new Blob([data.buffer], { type: "video/mp4" });
+
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+  a.download = "slider.mp4";
+  a.click();
+}
