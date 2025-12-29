0e2a1ce792463a9193064bee802a3dea3d069b13
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-30 00:17:44 +0300

    Update timeline.js

--- js/timeline.js
+++ js/timeline.js
@@ -1,15 +1,46 @@
 import { state } from "./state.js";
+import { renderSlider } from "./slider.js";
+import { pushHistory } from "./history.js";
 
-const container = document.getElementById("timeline-container");
+const timeline = document.getElementById("timeline");
 
 export function renderTimeline() {
-  container.innerHTML = "";
-  state.images.forEach((src, i) => {
-    const div = document.createElement("div");
-    div.className = "timeline-item";
-    div.innerHTML = `<img src="${src}"><input type="number" value="${state.durations[i] || 0.6}" step="0.1">`;
-    div.querySelector("input").onchange = e =>
-      state.durations[i] = parseFloat(e.target.value);
-    container.appendChild(div);
+  timeline.innerHTML = "";
+
+  state.images.forEach((img, i) => {
+    const item = document.createElement("div");
+    item.className = "timeline-item";
+    item.draggable = true;
+    item.dataset.index = i;
+
+    item.innerHTML = `
+      <img src="${img}">
+      <input type="number" step="0.1" value="${state.durations[i] || 0.6}">
+    `;
+
+    item.ondragstart = e => e.dataTransfer.setData("from", i);
+    item.ondragover = e => e.preventDefault();
+
+    item.ondrop = e => {
+      const from = +e.dataTransfer.getData("from");
+      const to = i;
+      reorder(from, to);
+      pushHistory();
+      renderTimeline();
+      renderSlider();
+    };
+
+    item.querySelector("input").oninput = e =>
+      state.durations[i] = +e.target.value;
+
+    timeline.appendChild(item);
+  });
+}
+
+function reorder(from, to) {
+  ["images", "texts", "positions", "durations"].forEach(key => {
+    const arr = state[key];
+    const item = arr.splice(from, 1)[0];
+    arr.splice(to, 0, item);
   });
 }
