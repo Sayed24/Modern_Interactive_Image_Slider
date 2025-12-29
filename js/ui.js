3dcb46602d3d60dc284ad153a05dd8d5a2d9d3b0
SayedRahim Sadat <56288293+Sayed24@users.noreply.github.com>
2025-12-30 00:22:47 +0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,38 +1,52 @@
 import { state } from "./state.js";
 import { saveImage } from "./storage.js";
-import { renderSlider, nextSlide, prevSlide } from "./slider.js";
+import { renderSlider, next, prev } from "./slider.js";
 import { renderTimeline } from "./timeline.js";
-import { exportAsGIF } from "./export.js";
-import { exportVideoWithMusic } from "./exportVideo.js";
+import { exportGIF } from "./exportGif.js";
+import { exportVideo } from "./exportVideo.js";
+import { exportMP4 } from "./exportMp4.js";
+import { pushHistory, undo, redo } from "./history.js";
 
 export function setupUI() {
 
-  document.getElementById("next").onclick = nextSlide;
-  document.getElementById("prev").onclick = prevSlide;
+  document.getElementById("next").onclick = () => {
+    next(); pushHistory();
+  };
+
+  document.getElementById("prev").onclick = () => {
+    prev(); pushHistory();
+  };
+
+  document.getElementById("undo").onclick = () => {
+    undo(); renderSlider(); renderTimeline();
+  };
+
+  document.getElementById("redo").onclick = () => {
+    redo(); renderSlider(); renderTimeline();
+  };
 
   document.getElementById("animation").onchange = e => {
     state.animation = e.target.value;
+    pushHistory();
     renderSlider();
   };
 
-  document.getElementById("theme").onchange = e => {
+  document.getElementById("themeSelect").onchange = e => {
     document.body.className = e.target.value;
     localStorage.setItem("theme", e.target.value);
   };
 
-  document.getElementById("file-input").onchange = e => {
+  document.getElementById("fileInput").onchange = e => {
     [...e.target.files].forEach(file => {
       saveImage(file);
       state.images.push(URL.createObjectURL(file));
     });
+    pushHistory();
     renderSlider();
     renderTimeline();
   };
 
-  document.getElementById("exportGif").onclick = exportAsGIF;
-
-  document.getElementById("exportVideo").onclick = () => {
-    const audio = document.getElementById("music-file").files[0];
-    if (audio) exportVideoWithMusic(audio);
-  };
+  document.getElementById("exportGif").onclick = exportGIF;
+  document.getElementById("exportWebm").onclick = exportVideo;
+  document.getElementById("exportMp4").onclick = exportMP4;
 }
